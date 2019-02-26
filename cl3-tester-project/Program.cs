6659f1742ed4f49929fe87a0f978b0d183b6c792
Amanda Kaufman <aka@microsoft.com>
2019-02-26 15:33:56 -0700

    Initial commit

--- cl3-tester-project/Program.cs
+++ cl3-tester-project/Program.cs
@@ -7,6 +7,13 @@ namespace cl3_tester_project
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("This project is intended to help CL3 ramp up on Git and version control.");
+            Console.WriteLine("Follow the steps in the Readme to start getting some hands-on practice!\n");
+
+            Console.WriteLine("Add your message with your name below the line :)");
+            Console.WriteLine("-----------------------------------------------------------------------\n");
+
+            // Add your messages here!
         }
     }
 }
