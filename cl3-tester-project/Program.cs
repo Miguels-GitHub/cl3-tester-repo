7bc2615480b2d79f1e4009cfec5d1fe04f06710f
Amanda Kaufman <aka@microsoft.com>
2019-02-26 15:30:58 -0700

    Add project files.

---
+++ cl3-tester-project/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace cl3_tester_project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
